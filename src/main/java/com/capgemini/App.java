2b034df8b61825c4919fe4da535431cd27cc5f4f
CarlijnR <37950298+CarlijnR@users.noreply.github.com>
2018-04-11 19:24:51 +0200

    Ingredient print fix

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -16,13 +16,17 @@ public class App {
         Drink drink = new Drink(1, EMenuItem.Drink, "Carlsberg", 2);
 
         Ingredient cream = new Ingredient("cream", 1, 500, 3);
+        Ingredient garlic = new Ingredient("garlic", 2, 500, 3);
+        Ingredient potato = new Ingredient("potato", 3, 500, 3);
+
         Food food = new Food(2, EMenuItem.Desert, "tiramisu", 3, cream);
-
-
+        Food food2 = new Food(3, EMenuItem.Desert, "icecream", 3, new Ingredient[]{cream, potato, garlic});
+        
         //print the drink and the food to the console
         //todo: the Ingredient is now presented as NULL, needs to be solved
         drink.printMenuItem();
         food.printMenuItem();
+        food2.printMenuItem();
 
 
     }
--- src/main/java/com/capgemini/Food.java
+++ src/main/java/com/capgemini/Food.java
@@ -5,26 +5,36 @@ public class Food extends MenuItem {
     //Food inherits from MenuItem, but also has an Ingredient or a list of Ingredients.
 
     private Ingredient[] listOfIngredients;
-    private Ingredient ingredient;
 
     public Food(int newId, EMenuItem newItem, String newName, double newPrice, Ingredient[] newListOfIngredients) {
         super(newId, newItem, newName, newPrice);
         this.listOfIngredients = newListOfIngredients;
     }
+
     public Food(int newId, EMenuItem newItem, String newName, double newPrice, Ingredient newIngredient) {
         super(newId, newItem, newName, newPrice);
-        this.ingredient = newIngredient ;
+
+        // maak een Ingredient array aan met size 1
+        Ingredient[] ingredients = new Ingredient[1];
+
+        // voeg het ingredient toe aan de nieuwe array
+        ingredients[0] = newIngredient;
+
+        // set de array op this.listOfIngredients
+        this.listOfIngredients = ingredients;
     }
 
 
-    public Ingredient[] getListOfIngredients() {
-        return this.listOfIngredients;
+    public void printListOfIngredients() {
+        for (Ingredient currentIngredient : this.listOfIngredients ) {
+            System.out.println(currentIngredient.getIngredientName());
+        }
     }
 
     @Override
     public void printMenuItem() {
         super.printMenuItem();
-        System.out.println(getListOfIngredients());
+        printListOfIngredients();
     }
 
 
