70aa6901d53f810d5fe17ebdcb4e62188de0ab77
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-04-11 10:08:50 +0200

    First basic menu in workable code
    First basic menu can be printed to the console.

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -4,6 +4,8 @@ package com.capgemini;
 Restaurant application
 */
 
+import java.sql.SQLOutput;
+
 public class App
 {
     private Menu menu;
@@ -13,8 +15,31 @@ public class App
 
     public static void main( String[] args )
     {
-        Ingredient listingredients = new Ingredient(1, "pasta", 200, 1.59f);
-        System.out.println(listingredients.getIngredient());
+        Ingredient ingredient1 = new Ingredient(1, "pasta", 200, 1.59f);
+        //System.out.println(ingredient1.getIngredient());
+
+        Food food1 = new Food(1, "pastadish", ingredient1);
+
+        Drink drink1 = new Drink(1, "vodka");
+
+        Menu menu = new Menu(1, "pagaplus Restaurant", food1, drink1);
+
+        System.out.println("This is the menu of " + menu.getMenuName());
+        System.out.println("******************");
+
+        System.out.println("These are the dishes of the menu: ");
+        System.out.println(food1.getFoodId() + " "  + food1.getFoodName());
+        System.out.println("******************");
+
+        System.out.println("These are the ingredients of "+ food1.getFoodName());
+        System.out.println(ingredient1.getIngredientId() + " " + ingredient1.getIngredientName());
+        System.out.println("******************");
+
+        System.out.println("These are the drinks of the menu");
+        System.out.println(drink1.getDrinkId() +" "+ drink1.getDrinkName());
+        System.out.println("******************");
+
+
 
     }
 }
