357e4cb9de71f97d1c2e858b788680bf102736f4
KevinTTT2310 <38212230+KevinTTT2310@users.noreply.github.com>
2018-04-12 16:36:28 +0200

    Added comments to App
    Ask if you dont understand

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -16,16 +16,21 @@ public class App {
         Ingredient cream = new Ingredient(4, "cream", 1500, 55);
 
 
+        //Menu is instantiated with a id, an ENum, name, price and Ingredients array
         MenuItem soup = new Food(1, EMenuItem.Starter, "soup", 2, new Ingredient[] {garlic, salt});
         MenuItem pizza = new Food(2, EMenuItem.Main, "pizza", 3, new Ingredient[] {cream});
         MenuItem tiramisu = new Food(3, EMenuItem.Desert, "tiramisu", 8, new Ingredient[]{garlic, oil, cream});
         MenuItem water = new Drink(4, EMenuItem.Drink, "water", 2);
 
+        //Instantiates a menu
         Menu menu = new Menu(new MenuItem[] {soup, pizza, tiramisu, water});
+
+        // A menu is created with a list of menu items, you need to add menu items after the MenuItem[]
 
         System.out.println("***Menu****");
         System.out.println();
 
+        // The menu is printed here. See Class menu for all details.
         menu.printMenuItemList();
 
 
