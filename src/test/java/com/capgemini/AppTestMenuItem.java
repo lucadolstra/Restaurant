07b338b6a4ed2af67878dcddb31ec4f899e6a258
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-04-16 16:22:59 +0200

    Addedt test for menuItem , including array
    Look at the array of ingredients in food,
    we dont have to cast it because we created a Food and not a MenuItem,
    we cannot compare an array against an array as is, but we can test the length of the arrays against each other

--- src/test/java/com/capgemini/AppTestMenuItem.java
+++ src/test/java/com/capgemini/AppTestMenuItem.java
@@ -1,35 +1,69 @@
 package com.capgemini;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AppTestMenuItem {
 
-    private static MenuItem testGetName;
+    private static MenuItem testMenuItemDrink;
+    private static Food testMenuItemFood;
 
-    @BeforeClass
-    public static void setup() {
-        testGetName = new MenuItem();
+//We do not use Before or BeforeClass because we use different constructors in every test
+
+
+    //Test the getName method of MenuItem and Drink
+    @Test
+    public void testGetDrinkName() {
+        int excpectedId = 1;
+        EMenuItem expectedItem = EMenuItem.Drink;
+        double expectedPrice = 1;
+        String expectedName = "Test";
+
+        testMenuItemDrink = new Drink(1, EMenuItem.Drink, "Test", 1);
+        String testName = testMenuItemDrink.getName();
+
+        assertEquals(testName, expectedName);
     }
 
-        @Test
-        public void testGetName() {
-            String name = "Burger";
 
-            this.testGetName.setName(name);
+    //Test the getName method for a Food MenuItem
+    @Test
+    public void testGetFoodName() {
+        Ingredient testIngredient = new Ingredient(3, "testIngredient", 200, 1.0);
+        Ingredient[] testListOfIngredients = {testIngredient};
 
-            assertEquals(this.testGetName.getName(), name);
-        }
+        int excpectedId = 2;
+        EMenuItem expectedItem = EMenuItem.Main;
+        double expectedPrice = 1;
+        String expectedName = "Test";
+        Ingredient[] expectedIngredientList = {testIngredient};
+        int expectedLength = expectedIngredientList.length;
 
+
+        testMenuItemFood = new Food(2, EMenuItem.Main, "Test", 1, testListOfIngredients);
+
+        String testName = testMenuItemFood.getName();
+        int testId = testMenuItemFood.getId();
+        double testPrice = testMenuItemFood.getPrice();
+
+//        if(testMenuItemFood instanceof Food) {
+//            Food temp = (Food)testMenuItemFood;
+//            temp.getListOfIngredients();
+//        }
+
+        Ingredient[] testIngredientList = testMenuItemFood.getListOfIngredients();
+        int testLength = testIngredientList.length;
+
+
+        assertEquals(testName, expectedName);
+        assertEquals(testId, excpectedId);
+        assertEquals(testPrice, expectedPrice);
+
+        //Because we test 2 arrays, we test the lengths of these arrays. Testing 2 arrays as is would not
+        //result in an equal result.
+        assertEquals(testLength, expectedLength);
     }
 
 }
 
-/*public String getName() {
-    return name;
-}*/
